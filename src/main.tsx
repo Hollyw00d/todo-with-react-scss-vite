5f3e6c69b1647c2623dd0d11217fde3073fc580b
Matt Jennings <matt@mattjennings.net>
2024-01-10 18:41:53 -0800

    added basic typescript, react, vite, scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './assets/global.scss'
-import {Provider} from "react-redux";
-import {store, persistor} from "./store/store.ts";
-import {PersistGate} from "redux-persist/integration/react";
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Provider store={store}>
-          <PersistGate persistor={persistor} loading={null}>
-              <App />
-          </PersistGate>
-      </Provider>
+    <App />
   </React.StrictMode>,
 )
